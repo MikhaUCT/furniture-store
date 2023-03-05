2b70821a2eef97c911498087c8c0d2a326623c0d
Mikha Davids <mikhadavids@gmail.com>
2023-03-05 14:34:43 +0200

    perf: :art: added image caching preprocessor & resized images

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
+import importAssets from 'svelte-preprocess-import-assets';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [
+		importAssets(),
+		vitePreprocess(),
+	],
 
 	kit: {
 		adapter: adapter(),
 		paths: {
 			base: dev ? "" : "/furniture-store",
+			// This allows me to have a base route of /furniture-store
 			relative: false,
 		},
 		prerender: {
